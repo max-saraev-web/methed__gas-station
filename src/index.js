aa9c6e6223ac78810792251fa3b92f75a8e380e6
unknown <max.saraev.web@gmail.com>
2024-02-13 20:58:50 +0800

    initial - commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import './style.css';
+
+const open = document.querySelector('.open');
+const car = document.querySelector('.car');
+
+const testArray = {
+  passangerCar: [
+    ['Opel', 'Crossland', 45],
+    ['Opel', 'Grandland X', 53],
+    ['Mazda', 'cx-5', 55],
+    ['BMW', 'M5', 68],
+    ['BMW', 'X5', 80],
+    ['BMW', 'X5d', 80, 'diesel'],
+    ['BMW', 'X3', 65],
+    ['BMW', '5', 66],
+  ],
+  truck: [
+    ['MAN', 'TGS', 400],
+    ['MAN', 'TGX', 300],
+    ['Mercedes-Benz', 'Actros', 450],
+    ['Mercedes-Benz', 'Actros L', 650],
+    ['Volvo', 'FH16', 700],
+    ['Volvo', 'FM', 700],
+    ['Volvo', 'FMX', 540],
+  ],
+};
+
+const getTestCar = () => {
+  const typeBool = Math.random() < 0.6;
+  const listCar = typeBool ? testArray.passangerCar : testArray.truck;
+  const randomCar = listCar[(Math.floor(Math.random() * listCar.length))];
+  return randomCar;
+};
+
+
+open.addEventListener('click', () => {
+  console.log('Открыто');
+  open.remove();
+  car.style.display = 'block';
+  car.addEventListener('click', () => {
+    console.log(getTestCar());
+  });
+});
+
+
